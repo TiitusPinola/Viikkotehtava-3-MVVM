f8d41e933150c580b7be95d380c164874ab73493
TiitusPinola <tiitus.pinola@gmail.com>
2026-01-29 12:09:25 +0200

    alustus

---
+++ app/src/main/java/com/example/viikkotehtava3/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.viikkotehtava3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.example.viikkotehtava3.view.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
